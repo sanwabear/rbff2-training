440c85196f8dc0c0858fb5f147a3b8d992036455
@ym2610(sanwabear) <sanwabear@hotmail.co.jp>
2019-05-09 23:21:08 +0900

    デフォルトの色を設定できるようにしました。

--- lib/rbff2-global.lua
+++ lib/rbff2-global.lua
@@ -130,7 +130,7 @@ gui_boxb = function(x1, y1, x2, y2, color1, color2, initb)
 		for y = y1, y2, stepy do
 			initb = not initb
 			if initb then
-				gui.drawpixel(x, y, color2)
+				gui.drawpixel(x, y, color2 or 0xCCCCFFFF)
 			end
 		end
 	end
