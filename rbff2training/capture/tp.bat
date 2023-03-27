31fa4dc48761edda7b4d7c7345017dd64f8c4706
@ym2610(sanwabear) <sanwabear@hotmail.co.jp>
2023-03-27 22:18:04 +0900

    スクリプト修正しました。

--- rbff2training/capture/tp.bat
+++ rbff2training/capture/tp.bat
@@ -1,2 +1,22 @@
+@echo off
 cd /d %~dp0
-FOR /R %%a IN (*.png) DO magick convert "%%~nxa" -strip -crop 320x216+0+4 -trim -transparent "#B0B6BE" "tp\%%~nxa"
+FOR /F "usebackq" %%a IN (`cd`) DO SET d1=%%a
+SET d2=%d1%\tp
+SET d3=
+setlocal enabledelayedexpansion
+
+FOR /R %%a IN (*.png) DO (
+	echo "%%a" | find "\tp\" >NUL
+	if ERRORLEVEL 1 (
+		set fpath=%%a
+		set fto=!!fpath:%d1%=%d2%!!
+		set fdir=!!fto:%%~nxa=%d3%!!
+		echo ------------
+		echo from !fpath!
+		echo to   !fto!
+		echo dir  !fdir!
+		if NOT exist "!fdir!" ( mkdir !fdir! )
+		magick convert "!fpath!" -strip -crop 320x216+0+4 -trim -transparent "#B0B6BE" "!fto!"
+	)
+)
+endlocal enabledelayedexpansion
