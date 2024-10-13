b6be567018653493c3c35278ec6394f031647cb4
@ym2610(sanwabear) <sanwabear@hotmail.co.jp>
2024-10-13 11:36:41 +0900

    CPU戦モード修正

--- rbff2training/rbff2.lua
+++ rbff2training/rbff2.lua
@@ -372,7 +372,7 @@ rbff2.startplugin  = function()
 			mem.w16(0x10E796, 0x0007) -- maincpu.pw@10E796=0008
 			]]
 			mem.wd32(0x050108, enabled and 0x303C0007 or 0x302D6016)
-			mem.wd32(0x050138, enabled and 0x3E3C0007 or 0x3E2D6792)
+			-- mem.wd32(0x050138, enabled and 0x3E3C0007 or 0x3E2D6792) -- CPU待ち時間用
 			mem.wd32(0x050170, enabled and 0x303C0007 or 0x302D6016)
 			mem.wd32(0x0501C8, enabled and 0x303C0007 or 0x302D6794)
 			mem.wd32(0x0501EE, enabled and 0x303C0007 or 0x302D6796)
@@ -385,6 +385,25 @@ rbff2.startplugin  = function()
 		sokaku_stg   = function(enabled)
 			-- 対戦の双角ステージをビリーステージに変更する（MVSと家庭用共通）
 			mem.wd16(0x00F290, enabled and 0x0004 or 0x0001)
+		end,
+		cpu_wait     = function(mode)
+			-- CPU動作間隔 052D94-052DD2
+			local org = {
+				0x0030, 0x0024, 0x001E, 0x0012, 0x002A, 0x001E, 0x0012, 0x000C, 0x0024, 0x0018, 0x000C,
+				0x0006, 0x0018, 0x0012, 0x000C, 0x0006, 0x0012, 0x000C, 0x0006, 0x0000, 0x000C, 0x0006,
+				0x0000, 0x0000, 0x0006, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+			}
+			for i, time in ipairs(org) do
+				-- mode 1:等倍 2:2分の1 3:3分の1 4:0
+				local addr = 0x052D94 + ((i - 1) * 2)
+				if mode == 4 then
+					mem.w16(addr, 0)
+				else
+					local newtime = math.floor(time >> (mode - 1))
+					--ut.printf("%X: %s > %s", addr, time, newtime)
+					mem.w16(addr, newtime)
+				end
+			end
 		end,
 		training     = function(tra)
 			mem.wd16(0x01F3BC, tra and 0x4E75 or 0x082C) -- 1Pのスコア表示をすぐ抜ける 082C
@@ -734,6 +753,7 @@ rbff2.startplugin  = function()
 		fix_skip_frame       = false,
 		proceed_cpu          = false, -- CPU戦進行あり
 		cpu_hardest          = true,-- CPU難度最高
+		cpu_wait             = 4, -- CPU待ち時間 1:等倍 2:2分の1 3:3分の1 4:0
 		cpu_stg              = true,-- CPUステージ
 		sokaku_stg           = true,-- 対戦双角ステージ
 		mvs_billy            = false,
@@ -1234,6 +1254,7 @@ rbff2.startplugin  = function()
 		mod.bugfix()
 		mod.training(true)
 		mod.cpu_hardest(global.cpu_hardest)
+		mod.cpu_wait(global.cpu_wait)
 		mod.cpu_stg(global.cpu_stg)
 		mod.sokaku_stg(global.sokaku_stg)
 		mod.snk_time(global.snk_time)
@@ -6173,6 +6194,8 @@ rbff2.startplugin  = function()
 			menu.organize_time_config(4, true) -- タイム設定=4:90
 			menu.organize_life_config(3) -- 体力ゲージモード=3:通常動作
 			g.pow_mode = 3            -- POWゲージモード=3:通常動作
+			g.cpu_wait = 3
+			mod.cpu_wait(g.cpu_wait)
 			g.sokaku_stg = false      -- 対戦双角ステージ
 			mod.sokaku_stg(global.sokaku_stg)
 			set_dip_config(true)
@@ -6186,6 +6209,8 @@ rbff2.startplugin  = function()
 			menu.organize_time_config(1, false) -- タイム設定=1:RB2(デフォルト)
 			menu.organize_life_config(1) -- 体力ゲージモード=1:自動回復
 			g.pow_mode = 2             -- POWゲージモード=2:固定
+			g.cpu_wait = 4
+			mod.cpu_wait(g.cpu_wait)
 			g.sokaku_stg = true        -- 対戦双角ステージ
 			mod.sokaku_stg(global.sokaku_stg)
 			set_dip_config(true)
@@ -6639,6 +6664,7 @@ rbff2.startplugin  = function()
 			{ "タイム設定(リスタートで反映)", { "無限:RB2(デフォルト)", "無限:RB2", "無限:SNK", "90", "60", "45", }, },
 			{ "CPU戦進行あり", menu.labels.off_on, },
 			{ "CPU難度最高", menu.labels.off_on, },
+			{ "CPU待ち時間", { "等倍", "2分の1", "4分の1", "0", }, },
 			{ "CPUステージ", { "通常", "対戦の2ラインステージ" }, },
 			{ "対戦双角ステージ", { "通常", "ビリーステージ" }, },
 		},
@@ -6663,8 +6689,9 @@ rbff2.startplugin  = function()
 			col[9] = g.time_mode        -- タイム設定 1:無限:RB2(デフォルト) 2:無限:RB2 3:無限:SNK 4:90 5:60 6:30
 			col[10] = g.proceed_cpu and 2 or 1 -- CPU戦進行あり
 			col[11] = g.cpu_hardest and 2 or 1 -- CPU難度最高
-			col[12] = g.cpu_stg and 2 or 1 -- CPUステージ
-			col[13] = g.sokaku_stg and 2 or 1 -- 対戦双角ステージ
+			col[12] = g.cpu_wait -- CPU待ち時間
+			col[13] = g.cpu_stg and 2 or 1 -- CPUステージ
+			col[14] = g.sokaku_stg and 2 or 1 -- 対戦双角ステージ
 		end,
 		ut.new_filled_table(13, function()
 			local col, g     = menu.extra.pos.col, global
@@ -6681,14 +6708,16 @@ rbff2.startplugin  = function()
 			-- CPU戦進行あり
 			menu.organize_time_config(col[9], col[10] == 2)
 			g.cpu_hardest = col[11] == 2 -- CPU難度最高
-			g.cpu_stg     = col[12] == 2 -- CPUステージ
-			g.sokaku_stg  = col[13] == 2 -- 対戦双角ステージ
+			g.cpu_wait    = col[12] -- CPU待ち時間
+			g.cpu_stg     = col[13] == 2 -- CPUステージ
+			g.sokaku_stg  = col[14] == 2 -- 対戦双角ステージ
 			mod.mvs_billy(g.mvs_billy)
 			mod.sadomazo_fix(g.sadomazo_fix)
 			mod.fix_skip_frame(g.fix_skip_frame)
 			set_dip_config(true)
 			menu.set_current()
 			mod.cpu_hardest(g.cpu_hardest)
+			mod.cpu_wait(g.cpu_wait)
 			mod.cpu_stg(g.cpu_stg)
 			mod.sokaku_stg(g.sokaku_stg)
 		end))
